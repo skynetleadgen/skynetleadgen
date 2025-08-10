b13bb6b76a6bd4be391b89cfd58b58e11b60c545
skynetleadgen <admin@skynetleadgen.com>
2025-08-10 18:10:54 +0400

    Create _manifest_.py

---
+++ skynetleadgen/subscription_manager/_manifest_.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Subscription Manager',
+    'version': '1.0',
+    'summary': 'Store software subscription details',
+    'category': 'Tools',
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [
+        'views/subscription_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
