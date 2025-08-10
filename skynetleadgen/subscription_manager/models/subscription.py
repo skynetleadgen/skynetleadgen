fc2ff1bdfc8dfa0c4d3c815e7232ece0dc3edf3c
skynetleadgen <admin@skynetleadgen.com>
2025-08-10 18:12:36 +0400

    Create subscription.py

---
+++ skynetleadgen/subscription_manager/models/subscription.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+class Subscription(models.Model):
+    _name = 'subscription.manager'
+    _description = 'Software Subscription'
+
+    name = fields.Char(string='Software Name', required=True)
+    user_id = fields.Char(string='User ID')
+    password = fields.Char(string='Password')
+    renewal_date = fields.Date(string='Renewal Date')
