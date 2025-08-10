99db7a763adc97f1e17cec6b5bde91cceac24ea3
skynetleadgen <admin@skynetleadgen.com>
2025-08-10 18:56:18 +0400

    Create phone_number.py

---
+++ skynetleadgen/phone_rating/models/phone_number.py
@@ -0,0 +1,14 @@
+from odoo import models, fields
+
+class PhoneNumber(models.Model):
+    _name = 'phone.number'
+    _description = 'Phone Number with Rating'
+
+    name = fields.Char(string='Phone Number', required=True)
+    rating = fields.Selection([
+        ('1', '1 Star'),
+        ('2', '2 Stars'),
+        ('3', '3 Stars'),
+        ('4', '4 Stars'),
+        ('5', '5 Stars'),
+    ], string='Rating', default='3')
