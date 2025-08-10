a78dd21bcee4b45cf6270d3558a6695d34b09969
skynetleadgen <admin@skynetleadgen.com>
2025-08-10 18:55:09 +0400

    Create __manifest__.py

---
+++ skynetleadgen/phone_rating/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Phone Number Ratings',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Store phone numbers and assign manual ratings',
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [
+        'views/phone_number_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
