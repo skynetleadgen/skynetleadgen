684ae69b7257ebb030fd2524a25118c8468a77a0
skynetleadgen <admin@skynetleadgen.com>
2025-08-10 19:00:23 +0400

    Update __manifest__.py

--- skynetleadgen/phone_rating/__manifest__.py
+++ skynetleadgen/phone_rating/__manifest__.py
@@ -6,6 +6,7 @@
     'author': 'Your Name',
     'depends': ['base'],
     'data': [
+        'security/ir.model.access.csv',
         'views/phone_number_views.xml',
     ],
     'installable': True,
