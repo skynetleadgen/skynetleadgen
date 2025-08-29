0afd73485e068482e8b60b135e464d7b3e11a2e8
skynetleadgen <admin@skynetleadgen.com>
2025-08-29 21:04:59 +0400

    Update __manifest__.py

--- skynetleadgen/manual_sales_commission/__manifest__.py
+++ skynetleadgen/manual_sales_commission/__manifest__.py
@@ -9,8 +9,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/sales_commission_menu.xml',
-        'report/sales_commission_report.xml',   # <-- IMPORTANT
-    ],
+        ],
     'installable': True,
     'application': True,
     'auto_install': False,
