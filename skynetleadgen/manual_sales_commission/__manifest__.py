9ab47f2c57b6d171dc10a1144f5d9bf667415bef
skynetleadgen <admin@skynetleadgen.com>
2025-08-29 20:57:31 +0400

    Update __manifest__.py

--- skynetleadgen/manual_sales_commission/__manifest__.py
+++ skynetleadgen/manual_sales_commission/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Manual Sales Commission',
-    'version': '1.0',
+    'version': '1.1',
     'summary': 'Manage manual sales commissions',
     'description': 'Module to handle manual sales commissions',
     'author': 'Your Name',
