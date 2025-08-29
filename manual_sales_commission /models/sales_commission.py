46d436cc4c110dd2d06ee802e8c1c7f6833e5c38
skynetleadgen <admin@skynetleadgen.com>
2025-08-29 19:55:26 +0400

    Create sales_commission.py

---
+++ manual_sales_commission /models/sales_commission.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api
+
+class ManualSalesCommission(models.Model):
+    _name = "manual.sales.commission"
+    _description = "Manual Sales Commission"
+
+    name = fields.Char(string="Salesperson Name", required=True)
+    no_of_leads = fields.Integer(string="Number of Leads", required=True)
+    commission_amount = fields.Float(
+        string="Commission Amount",
+        compute="_compute_commission",
+        store=True
+    )
+
+    @api.depends('no_of_leads')
+    def _compute_commission(self):
+        for record in self:
+            record.commission_amount = record.no_of_leads * 500
